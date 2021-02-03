cca53f651f94eecbc4f83b52ed50d65c659805a2
Ann <ber.anny@yandex.ru>
2021-02-03 15:25:37 +0300

    Completed Task3 / Homework 2

--- src/LessonTwo.java
+++ src/LessonTwo.java
@@ -5,6 +5,7 @@ public class LessonTwo {
     public static void main(String[] args){
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -20,12 +21,23 @@ public class LessonTwo {
         int[] theThird = new int[8];
         theThird[0] = 1;
         for (int i = 1; i < 8; i++) {
-            theThird[i] = theThird[i-1]+3;
+            theThird[i] = theThird[i - 1] + 3;
         }
         System.out.println(Arrays.toString(theThird));
     }
 
     public static void task3() {
-
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int[] res= multiplay(arr);
+        System.out.println(Arrays.toString(res));
+    }
+    public static int[] multiplay(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] *= 2;
+            }
+        }
+        return arr;
     }
 }
+
